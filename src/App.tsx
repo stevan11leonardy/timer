ed9bf15c85dfdcc7d09ff12041b68cc67ea9191f
stevan11leonardy <stevan11220@gmail.com>
2019-09-15 22:21:39 +0700

    add play button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import PlayIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
 import './App.css';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
+const App: React.FC = () => {
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(0);
+  const [intervalId, setIntervalId] = useState();
+
+  function startTimer(): void{
+    const timeRemaining = minutes * 60 + seconds;
+    let minute = Math.floor(timeRemaining / 60);
+    let second = timeRemaining;
+
+    const interval = setInterval(() => {
+      if (minute !== 0) {
+        setMinutes(minute -= 1);
+      }
+      if (second !== 0) {
+        setSeconds(second -= 1);
+      }
+    }, 1000);
+    setIntervalId(interval);
+  }
+
+  function stopTimer(): void {
+    clearInterval(intervalId);
+  }
+
+  function handlePlayButton() {
+    startTimer();
+  }
+
+  return (
+    <Grid container className="container">
+      <Grid item md={12}>
+        <Typography
+          variant="h1"
+          align="center"
+          className="timer-text"
+        >
+          {`${minutes}:${seconds}`}
+        </Typography>
+      </Grid>
+      <IconButton
+        className="icon-btn"
+        onClick={handlePlayButton}
       >
-        Learn Reac
-      </a>
-    </header>
-  </div>
-);
+        <PlayIcon style={{ fontSize: 60, color: 'black' }}/>
+      </IconButton>
+      <IconButton
+        className="icon-btn"
+        onClick={stopTimer}
+      >
+        <StopIcon style={{ fontSize: 60, color: 'black' }}/>
+      </IconButton>
+    </Grid>
+  );
+};
 
 export default App;
